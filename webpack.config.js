2ada6a92b12482a99131b255603f0603f30dfeb3
Thadeu Esteves Jr <tadeuu@gmail.com>
2019-05-31 18:40:09 -0300

    fix build minified

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,45 @@
-const path = require('path');
+const pkg = require("./package.json");
+const webpack = require("webpack");
+const path = require("path");
+
 const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+
 let filename;
 let plugins = [];
 let mode;
 
-if (process.env.NODE_ENV === "production") {
-  mode = "production";
-  filename = "logger-storage.min.js";
-  plugins.push(new UglifyJSPlugin({ sourceMap: true }));
-} else {
-  mode = "development";
-  filename = "logger-storage.js";
-}
+module.exports = () => {
+  if (process.env.NODE_ENV === "production") {
+    mode = "production";
+    filename = "logger-storage.min.js";
+    plugins.push(new UglifyJSPlugin({ sourceMap: true }));
+  } else {
+    mode = "development";
+    filename = "logger-storage.js";
+  }
 
-module.exports = {
-  entry: path.resolve(__dirname, "src/logger-storage.js"),
-  devtool: "source-map",
-  target: "web",
-  node: {
-    fs: "empty"
-  },
-  mode: mode,
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: filename,
-    library: "loggerStorage"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: /src/,
-        exclude: /node_modules/,
-        use: ["babel-loader"]
-      }
-    ]
-  },
-  plugins: plugins
-}
+  return {
+    entry: path.resolve(__dirname, "src/logger-storage.js"),
+    devtool: "source-map",
+    target: "web",
+    node: {
+      fs: "empty"
+    },
+    mode: "production",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: filename,
+      library: "loggerStorage"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          include: /src/,
+          exclude: /node_modules/,
+          use: ["babel-loader"]
+        }
+      ]
+    }
+  };
+};
